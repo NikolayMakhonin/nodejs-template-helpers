16189403aa60d8a2ec29e12459d124fda5390eac
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-01-27 11:38:38 +0200

    CI: karma BrowserStack

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			// 'node_modules/babel-polyfill/tmp/polyfill.js',
+			helpers.servedPattern(require.resolve('babel-polyfill/dist/polyfill')),
 			helpers.servedPattern(require.resolve('chai/chai')),
 			helpers.servedPattern(helpers.writeTextFile('tmp/karma/chai.js', '"use strict"; var assert = chai.assert, expect = chai.expect, should = chai.should;')),
 			helpers.concatJsFiles(
